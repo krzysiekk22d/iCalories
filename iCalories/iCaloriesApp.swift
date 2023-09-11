739ceeb434ecac01ed025e92e59b01f7e67ef9c3
krzysiekk22d <klas.club@gmail.com>
2023-09-11 19:36:55 +0200

    Initial Commit

---
+++ iCalories/iCaloriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  iCaloriesApp.swift
+//  iCalories
+//
+//  Created by Krzysztof Czura on 11/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct iCaloriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
